41dff9969d5dfed4e42353b3c5276fc293068f51
Andy Truong <thehongtt@gmail.com>
2014-12-29 17:49:46 +0700

    Migrate quiz answers.

---
+++ src/Destination/AnswerDestination.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\quizz_migrate\Destination;
+
+use Drupal\quizz_migrate\BaseEntityDestination;
+
+class AnswerDestination extends BaseEntityDestination {
+
+  protected $entity_type = 'quiz_result_answer';
+  protected $base_table = 'quiz_results_answers';
+  protected static $pk_name = 'result_answer_id';
+  protected $disable_import_complete = TRUE;
+
+}
