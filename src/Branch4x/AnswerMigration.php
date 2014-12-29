11217b19f36236c7cbf5d36fdb22c4839a8fed0b
Andy Truong <thehongtt@gmail.com>
2014-12-29 17:59:54 +0700

    Migrate quiz answers (continue)

--- src/Branch4x/AnswerMigration.php
+++ src/Branch4x/AnswerMigration.php
@@ -25,7 +25,6 @@ class AnswerMigration extends Migration {
 
   protected function setupMigrateSource() {
     $query = db_select('quiz_node_results_answers', 'answer');
-    $query->innerJoin('node_revision', 'r', 'answer.question_vid = r.vid');
     $query->innerJoin('node', 'n', 'answer.question_nid = n.nid');
     $query->addField('n', 'type', 'question_type');
     $query
@@ -35,6 +34,11 @@ class AnswerMigration extends Migration {
       ->condition('n.type', array_keys(quizz_question_get_handler_info()))
       ->orderBy('r.vid')
     ;
+
+    // Do not migrate broken revisions
+    $query->innerJoin('node_revision', 'r', 'answer.question_vid = r.vid');
+    $query->innerJoin('quiz_node_results', 'result', 'answer.result_id = result.result_id');
+
     return new MigrateSourceSQL($query);
   }
 
@@ -48,9 +52,11 @@ class AnswerMigration extends Migration {
     $this->addSimpleMappings(array('points_awarded', 'answer_timestamp', 'is_doubtful'));
     $this->addFieldMapping('question_qid', 'question_nid');
     $this->addFieldMapping('question_vid', 'question_vid');
+    $this->addFieldMapping('type', 'question_type');
   }
 
   public function prepare($answer, $row) {
+    $answer->type = $row->question_type;
     $map = array(
         'result_id'    => 'SELECT destid1 FROM {migrate_map_quiz_result} WHERE sourceid1 = :id',
         'question_qid' => 'SELECT destid1 FROM {migrate_map_quiz_question__' . $row->question_type . '} WHERE sourceid1 = :id',
