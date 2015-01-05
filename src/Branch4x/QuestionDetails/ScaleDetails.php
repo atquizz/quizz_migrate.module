a6a0f9d40cb6d0a27090221c744edc70538fbf58
Andy Truong <thehongtt@gmail.com>
2015-01-05 10:09:22 +0700

    Replace 'answer_collection_id' with 'collection_id'.

--- src/Branch4x/QuestionDetails/ScaleDetails.php
+++ src/Branch4x/QuestionDetails/ScaleDetails.php
@@ -12,16 +12,16 @@ class ScaleDetails extends BaseDetails {
   protected $column_mapping = array(
       'nid'                  => 'qid',
       'vid'                  => 'vid',
-      'answer_collection_id' => 'answer_collection_id'
+      'answer_collection_id' => 'collection_id'
   );
 
   public function prepare($item, $row) {
     parent::prepare($item, $row);
 
     $sql = 'SELECT destid1 FROM {migrate_map_quiz_scale_collection} WHERE sourceid1 = :id';
-    $item->answer_collection_id = db_query($sql, array(':id' => $row->answer_collection_id))->fetchColumn();
-    if ($item->answer_collection_id) {
-      throw new RuntimeException('Can not find item.answer_collection_id');
+    $item->collection_id = db_query($sql, array(':id' => $row->answer_collection_id))->fetchColumn();
+    if ($item->collection_id) {
+      throw new RuntimeException('Can not find item.collection_id');
     }
   }
 
