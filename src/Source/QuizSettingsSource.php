eacb97b06875da83f86a74f7859e5e61cf90184a
Andy Truong <thehongtt@gmail.com>
2015-01-05 17:01:53 +0700

    Define quiz settings source.

---
+++ src/Source/QuizSettingsSource.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Drupal\quizz_migrate\Source;
+
+use MigrateSource;
+
+class QuizSettingsSource extends MigrateSource {
+
+  private $quiz_type;
+  private $item_number = 0;
+  private $variables = array();
+
+  public function __construct($options = array()) {
+    $this->quiz_type = $options['quiz_type'];
+    parent::__construct($options);
+
+    $this->variables = array(
+        array('quiz_default_close', 30),
+        array('quiz_use_passfail', 1),
+        array('quiz_max_result_options', 5),
+        array('quiz_remove_partial_quiz_record', 604800), // 7 days
+        array('quiz_pager_start', 100),
+        array('quiz_pager_siblings', 5),
+    );
+
+    foreach ($this->variables as $k => $v) {
+      $this->variables[$k] = array($v[0], variable_get($v[0], $v[1]));
+    }
+  }
+
+  public function fields() {
+    return array(
+        'name'  => 'Variable name',
+        'value' => 'Varible value',
+    );
+  }
+
+  public function __toString() {
+    return "Settings for quiz:{$this->quiz_type}";
+  }
+
+  public function computeCount() {
+    return count($this->variables);
+  }
+
+  public function performRewind() {
+    $this->item_number = 0;
+  }
+
+  public function getNextRow() {
+    if (!isset($this->variables[$this->item_number])) {
+      return NULL;
+    }
+
+    $var = $this->variables[$this->item_number++];
+    return (object) array('name' => $var[0], 'value' => $var[1]);
+  }
+
+}
